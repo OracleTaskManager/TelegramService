72777596f33e6e9d4cfc23903c027c7673392a5e
Piga962 <cesarjrmecinas@gmail.com>
2025-06-09 17:02:56 -0600

    Hola yo soy Mateo

--- src/main/java/com/Oracle/TelegramService/config/RestTemplateConfig.java
+++ src/main/java/com/Oracle/TelegramService/config/RestTemplateConfig.java
@@ -1,8 +1,15 @@
 package com.Oracle.TelegramService.config;
 
+import feign.Client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.SecureRandom;
+import java.security.cert.X509Certificate;
 
 @Configuration
 public class RestTemplateConfig {
@@ -10,6 +17,29 @@ public class RestTemplateConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public Client feignClient() {
+        return new Client.Default(getTrustAllSSLSocketFactory(),
+                (hostname, session) -> true);
+    }
+
+    private javax.net.ssl.SSLSocketFactory getTrustAllSSLSocketFactory() {
+        try {
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+                @Override
+                public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                @Override
+                public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+            }}, new SecureRandom());
+            return sslContext.getSocketFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Error configurando SSL para Feign", e);
+        }
     }
 
 }
